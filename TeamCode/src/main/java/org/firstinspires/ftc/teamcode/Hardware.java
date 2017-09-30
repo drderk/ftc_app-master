4f8968d4ea2c0cde704649519826ed5a05cc40f8
Pureawesomeness <dperdomo13@yahoo.com>
2017-09-30 15:03:01 -0400

    Added init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -91,14 +91,14 @@ public class Hardware
         // Define and Initialize Motors
         leftDrive  = hwMap.get(DcMotor.class, "left_drive");
         rightDrive = hwMap.get(DcMotor.class, "right_drive");
-        //collect  = hwMap.get(DcMotor.class, "collect");
-        //lift  = hwMap.get(DcMotor.class, "lift");
-        //extend  = hwMap.get(DcMotor.class, "extend");
-        //rotate  = hwMap.get(Servo.class, "rotate");
-        //jewel  = hwMap.get(Servo.class, "jewel");
-        //grab  = hwMap.get(Servo.class, "grab");
-        //colorSensor = hwMap.get(ColorSensor.class, "colorSensor");
-        //breakBeam = hwMap.get(SensorDIO.class, "breakBeam");
+//        collect  = hwMap.get(DcMotor.class, "collect");
+//        lift  = hwMap.get(DcMotor.class, "lift");
+//        extend  = hwMap.get(DcMotor.class, "extend");
+//        rotate  = hwMap.get(Servo.class, "rotate");
+//        jewel  = hwMap.get(Servo.class, "jewel");
+//        grab  = hwMap.get(Servo.class, "grab");
+//        colorSensor = hwMap.get(ColorSensor.class, "colorSensor");
+//        breakBeam = hwMap.get(SensorDIO.class, "breakBeam");
 
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
@@ -111,15 +111,15 @@ public class Hardware
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        extend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        collect.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        extend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        collect.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        extend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        collect.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        extend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        collect.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
     }
