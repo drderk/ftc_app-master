fed95bfa58e1b838845e01f00619c6472d0b071f
Pureawesomeness <dperdomo13@yahoo.com>
2017-09-30 14:51:15 -0400

    Added init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -89,7 +89,6 @@ public class Hardware
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-<<<<<<< HEAD
         leftDrive  = hwMap.get(DcMotor.class, "left_drive");
         rightDrive = hwMap.get(DcMotor.class, "right_drive");
         collect  = hwMap.get(DcMotor.class, "collect");
@@ -98,18 +97,9 @@ public class Hardware
         rotate  = hwMap.get(Servo.class, "rotate");
         jewel  = hwMap.get(Servo.class, "jewel");
         grab  = hwMap.get(Servo.class, "grab");
-        leftDrive  = hwMap.get(DcMotor.class, "leftDrive");
-        rightDrive = hwMap.get(DcMotor.class, "rightDrive");
-        collect = hwMap.get(DcMotor.class, "collect");
-        lift = hwMap.get(DcMotor.class, "lift");
-        extend = hwMap.get(DcMotor.class, "extend");
-        rotate = hwMap.get(Servo.class, "rotate");
-        jewel = hwMap.get(Servo.class, "jewel");
-        grab = hwMap.get(Servo.class, "grab");
         colorSensor = hwMap.get(ColorSensor.class, "colorSensor");
         breakBeam = hwMap.get(SensorDIO.class, "breakBeam");
 
->>>>>>> 2865faabd27867063fd87405b34db97eb18d0488
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
