f5fe15d1197645f0ebdad08fabf70e0f3621813a
Pureawesomeness <dperdomo13@yahoo.com>
2017-09-30 09:19:16 -0400

    9/30/17

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
@@ -0,0 +1,187 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation;
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.teamcode.PinkNavigate;
+
+/**
+ * This file illustrates the concept of driving a path based on encoder counts.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByTime;
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forwards, and causes the encoders to count UP.
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backwards for 24 inches
+ *   - Stop and close the claw.
+ *
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This methods assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Auto Drive", group="Pushbot")
+public class AutoDrive extends LinearOpMode {
+
+    //declare variables
+    Hardware         robot   = new Hardware();   // Use a Pushbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
+    BNO055IMU imu;
+    int currentAngle;
+    int stage = 0;
+    boolean auto = true;
+
+    VuMarks camera = new VuMarks();
+    RelicRecoveryVuMark picturePos;
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        PinkNavigate.robot = robot;
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        //Motion Start
+        while (auto) {
+            currentAngle = (int)GetHeading();
+        switch (stage) {
+            case 0 : //initialize
+                robot.leftDrive.setPower(0);
+                robot.rightDrive.setPower(0);
+                robot.collect.setPower(0);
+                robot.lift.setPower(0);
+                stage = 0;
+                break;
+
+            case 1: //scan surroundings
+                picturePos = camera.vuMark;
+                telemetry.addData("Glyph pos", picturePos);
+                telemetry.addData("Jewel Color", jewelColor());
+                stage = 2;
+                break;
+
+            case 2:
+                if (PinkNavigate.driveToPos(45, 0, currentAngle, 0, 0, 1)) {
+                    stage = 1;
+                } else {
+                    stage = 0;
+                }
+                telemetry.addData("Stage", stage);
+                break;
+
+            case 3:
+                PinkNavigate.driveToPos(45, 0, currentAngle, 0, 0, 1);
+                auto = false;
+                break;
+
+        }
+        }
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+    /*
+     *  Method to perfmorm a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+   */
+
+    public double GetHeading(){
+        Orientation angles;
+        angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle);
+    }
+    public  String jewelColor() {
+        if (robot.colorSensor.red() > 3)
+            return "Red";
+        else{
+            return "Blue";
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinkNavigate.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.teamcode.Hardware;
+/**
+ * Created by Andy on 11/13/2015.
+ */
+
+public class PinkNavigate
+{
+    static Hardware robot = new Hardware();
+
+
+    static final double COUNTS_PER_INCH = 1000;  // Base travel
+    static final double POSITION_THRESHOLD = 10.0;   // Counts
+    static final double ANGLE_THRESHOLD = 5.0;     // Degrees
+
+    // Tank drive two wheels to target positions in inches.
+    // Returns true when both arrive at the target.
+    public static boolean driveToPos(double targetPos, double targetAngle, int currentAngle,
+    double linearVelocity, double angularVelocity, double maxPower)
+    {
+        double motorCmd = 0;
+        double targetPosCounts = targetPos * COUNTS_PER_INCH;
+        double leftMotorCmd, rightMotorCmd;
+        double leftWheelPos = robot.rightDrive.getCurrentPosition();
+        double rightWheelPos = robot.leftDrive.getCurrentPosition();
+        double angleErrorDegrees = targetAngle - currentAngle;
+        double currentPosCounts = (leftWheelPos + rightWheelPos)/2.0;
+        double angleOffset;
+        double linearError = targetPosCounts - currentPosCounts;
+        double angularError = targetAngle - currentAngle;
+
+        // Determine the baseline motor speed command
+        motorCmd = Range.clip(motorCmd, -0.5, 0.5);
+
+        // Determine and add the angle offset
+        angleOffset = PinkPD.getMotorCmd(0.02, 0.02, angularError, angularVelocity);
+        leftMotorCmd = motorCmd + angleOffset;
+        rightMotorCmd = motorCmd - angleOffset;
+        leftMotorCmd = Range.clip(leftMotorCmd, -1.0, 1.0);
+        rightMotorCmd = Range.clip(rightMotorCmd, -1.0, 1.0);
+
+        // Scale the motor commands back to account for the MC windup problem
+        // (if the motor cant keep up with the command, error builds up)
+        leftMotorCmd *= maxPower;
+        rightMotorCmd *= maxPower;
+
+        robot.leftDrive.setPower(leftMotorCmd);
+        robot.rightDrive.setPower(rightMotorCmd);
+        if((Math.abs(linearError)<POSITION_THRESHOLD)&&(Math.abs(angleErrorDegrees)<ANGLE_THRESHOLD))
+        {
+             return false;
+        } else
+        {
+            return true;
+        }
+    }
+
+    public static void stopBase()
+    {
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
+    }
+
+    public static void runUsingEncoders()
+    {
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public static void runWithoutEncoders()
+    {
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public static boolean resetBasePosition()
+    {
+        boolean resetStatus = false;
+
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        if ((robot.leftDrive.getCurrentPosition()==0)&&(robot.rightDrive.getCurrentPosition()==0))
+        {
+            resetStatus = true;
+        }
+        return resetStatus;
+    }
+
+    public static double getBasePosition()
+    {
+        return ((robot.leftDrive.getCurrentPosition() + robot.rightDrive.getCurrentPosition())/2.0);
+    }
+
+    /**
+     * Created by Andy on 11/12/2015.
+     *
+     * Both motors and continuous servos are covered.
+     */
+}
